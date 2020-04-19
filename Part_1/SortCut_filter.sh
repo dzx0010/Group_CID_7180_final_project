f31aaedd7c343e477d40d7d1541cd11bd96b523c
Isaac McNeely <imcneely@gmail.com>
2020-04-19 11:06:50 -0500

    New filter script for separating country data

---
+++ Part_1/SortCut_filter.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Print 2 useful columns to temp file
+
+awk -F "," '{print $6";"$1}' COVID_Seqs.csv > Testawk3.txt 
+
+#remove " from country names
+
+cat Testawk3.txt | tr -d '"' > Accessions_COVID.txt
+
+echo "List ready to sort"
+
+#sort based on country
+#sort -d --sort="China" -o China_sorted.txt COVID_Seqs.csv
+
+#grep for each country
+grep -e "China" Accessions_COVID.txt > China_sorted.txt
+grep -e "China:" Accessions_COVID.txt > China_sites_sorted.txt
+echo "China sorted"
+
+
+grep -e "USA" Accessions_COVID.txt > USA_sorted.txt
+grep -e "USA:" Accessions_COVID.txt > USA_states_sorted.txt
+echo "USA sorted"
+
+grep -f Euro_Countries.txt Accessions_COVID.txt > Euro_sorted.txt
+grep -f Asia_Countries.txt Accessions_COVID.txt > Asia_sorted.txt
+grep -f Other_countries.txt Accessions_COVID.txt > Other_sorted.txt 
+echo "-------------------------------"
+
+#echo *_sorted.txt > Sorted_files.txt
+
+#FILELIST=`cat Sorted_files.txt`
+#For FILE in $FILELIST
+#do 
+###randomize locations in each file
+#sort -u -R $FILE 
+
+#cut country from each list
+ 
+#cut -f 2 -d ';' $FILE
+#done
+#echo "Sorted list of accessions ready"
+
